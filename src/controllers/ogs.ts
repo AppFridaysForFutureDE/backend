f9a1144e901cc10aacd86197d32195948d42dd54
Nikolai Kircher <nikolai.kircher@konen.de>
2020-03-14 17:02:20 +0100

    save ogs to mongoose db

--- src/controllers/ogs.ts
+++ src/controllers/ogs.ts
@@ -1,16 +1,46 @@
 import { RequestHandler } from 'express';
 import { Og } from '../models/ogs'
+// import { Document, Model, model, Types, Schema, Query } from "mongoose"
+const mongoose = require('mongoose');
 
-// TODO: Use db instead
-const OGS: Og[] = [];
+// connect to Mongo daemon
+// TODO: Move this to another file
+mongoose
+  .connect(
+    'mongodb://fffapp:fffapp@mongo-db:27017/fffapp',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err: Error) => console.log(err));
+
+
+// DB schema
+const ItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+const Item = mongoose.model('item', ItemSchema);
 
 export const createOg: RequestHandler = (req, res, next) => {
-  const name = (req.body as {text: string}).text;
-  const newOg = new Og(Math.random().toString(), name);
-  OGS.push(newOg);
-  res.status(201).json({message: 'Created og', createdOg: newOg});
+  // const name = (req.body as {text: string}).text;
+  // const newOg = new Og(Math.random().toString(), name);
+
+  const newItem = new Item({
+    name: req.body.text
+  });
+
+  newItem.save().then(() => {
+      res.status(201).json({message: 'Created og', createdOg: req.body.text});
+    });   
 };
 
 export const getOgs: RequestHandler = (req, res, next) => {
-    res.status(200).json(OGS);
-  };
+  Item.find(function (err: Error, items: any) {
+    if (err) return console.error(err);
+    console.log(items);
+    res.status(200).json(items);
+  })
+};
