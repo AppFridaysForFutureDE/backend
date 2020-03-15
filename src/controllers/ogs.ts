178d727076649df7ea29b1149ae4b9cd4f6a79ae
Nikolai Kircher <nikolai.kircher@konen.de>
2020-03-15 10:11:36 +0100

    verschiebe og schema in model

--- src/controllers/ogs.ts
+++ src/controllers/ogs.ts
@@ -1,36 +1,19 @@
 import { RequestHandler } from 'express';
 import { Og } from '../models/ogs'
-// import { Document, Model, model, Types, Schema, Query } from "mongoose"
-const mongoose = require('mongoose');
 
+export const createOg: RequestHandler = (req, res) => {
+  const ogName = (req.body as {text: string}).text;
+  const newOg = new Og({name: ogName});
 
-// DB schema
-const ItemSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  }
-});
-
-const Item = mongoose.model('item', ItemSchema);
-
-export const createOg: RequestHandler = (req, res, next) => {
-  // const name = (req.body as {text: string}).text;
-  // const newOg = new Og(Math.random().toString(), name);
-
-  const newItem = new Item({
-    name: req.body.text
-  });
-
-  newItem.save().then(() => {
-      res.status(201).json({message: 'Created og', createdOg: req.body.text});
+  newOg.save().then(() => {
+      res.status(201).json({message: 'Created og', createdOg: ogName});
     });   
 };
 
-export const getOgs: RequestHandler = (req, res, next) => {
-  Item.find(function (err: Error, items: any) {
+export const getOgs: RequestHandler = (req, res) => {
+  Og.find(function (err: Error, ogs) {
     if (err) return console.error(err);
-    console.log(items);
-    res.status(200).json(items);
+    console.log(ogs);
+    res.status(200).json(ogs);
   })
 };
--- src/models/ogs.ts
+++ src/models/ogs.ts
@@ -1,3 +1,10 @@
-export class Og {
-    constructor(public id: string, public name: string) {}
-}
+import mongoose from 'mongoose';
+
+const OgSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    }
+  });
+
+export const Og = mongoose.model('og', OgSchema);
