facb1e92827cc51fb33657d89ccf3f635d5ab130
Moritz Tietze <moritz.tietze@icloud.com>
2020-03-20 15:25:16 +0100

    Prettier hinzugefügt
    hab prettier hinzugefügt

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,15 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: [
       '@typescript-eslint',
+      'prettier'
     ],
     extends: [
       'eslint:recommended',
       'plugin:@typescript-eslint/eslint-recommended',
       'plugin:@typescript-eslint/recommended',
+      'prettier'
     ],
+    rules: [
+      'prettier/prettier': 'error'
+    ]
   };
