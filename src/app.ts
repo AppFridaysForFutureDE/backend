c0798f3b031f3efac3b68b744f64923f56c6548a
Nikolai Kircher <nikolai.kircher@konen.de>
2020-03-22 10:25:48 +0100

    auto correct code style
    npm run lint-fix

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,36 @@
-import express, {Request, Response} from 'express';
-import { json } from 'body-parser';
-import ogRoutes from './routes/ogs';
-import mongoose from 'mongoose';
-const Ddos = require('ddos');
-const ddos = new Ddos({burst:10, limit:15});//probably need to adjust these
+import express, { Request, Response } from "express";
+import { json } from "body-parser";
+import ogRoutes from "./routes/ogs";
+import mongoose from "mongoose";
+const Ddos = require("ddos");
+const ddos = new Ddos({ burst: 10, limit: 15 }); //probably need to adjust these
 
-var mongoUp: boolean = true;
+let mongoUp = true;
 
 //create server, add json encoding and ddos protection
 const app = express();
 app.use(json());
 
-
 // connect to Mongo daemon
 mongoose
-  .connect(
-    'mongodb://fffapp:fffapp@mongo-db:27017/fffapp',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
+  .connect("mongodb://fffapp:fffapp@mongo-db:27017/fffapp", {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB Connected"))
   .catch(function(err) {
     console.log(err);
-    mongoUp = false;//cant use close() here because server isnt already listening
-    console.log("Server not started because of a critical error that occurred when starting MongoDB");
+    mongoUp = false; //cant use close() here because server isnt already listening
+    console.log(
+      "Server not started because of a critical error that occurred when starting MongoDB"
+    );
   });
 
 //initialise routers; every router needs to use ddos
 ogRoutes.use(ddos.express);
-app.use('/ogs', ogRoutes);
+app.use("/ogs", ogRoutes);
 
 app.use((err: Error, req: Request, res: Response) => {
-  res.status(500).json({message: err.message});
+  res.status(500).json({ message: err.message });
 });
 
 //only start server when mongo is up
--- src/controllers/ogs.ts
+++ src/controllers/ogs.ts
@@ -1,19 +1,19 @@
-import { RequestHandler } from 'express';
-import { Og } from '../models/ogs'
+import { RequestHandler } from "express";
+import { Og } from "../models/ogs";
 
 export const createOg: RequestHandler = async (req, res) => {
   try {
-    let newOg = await new Og(req.body).save()
-    res.status(201).json({message: 'Created og', createdOg: newOg});
+    const newOg = await new Og(req.body).save();
+    res.status(201).json({ message: "Created og", createdOg: newOg });
   } catch (error) {
-    res.status(422).json({message: 'Could not create og', error: error});
+    res.status(422).json({ message: "Could not create og", error: error });
   }
 };
 
 export const getOgs: RequestHandler = (req, res) => {
-  Og.find(function (err: Error, ogs) {
+  Og.find(function(err: Error, ogs) {
     if (err) return console.error(err);
     console.log(ogs);
     res.status(200).json(ogs);
-  })
+  });
 };
