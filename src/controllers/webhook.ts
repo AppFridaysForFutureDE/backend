40aa7b52739fa738f0c75d27b1f57d13a74cb4a4
Moritz Tietze <moritz.tietze@icloud.com>
2020-03-29 20:47:22 +0200

    Update webhook.ts

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -6,8 +6,9 @@ export const webhookTriggered: RequestHandler = (req, res) => {
   var data = req.body();
   var title = data["post"]["current"]["title"];
   var tag = data["post"]["current"]["primary_tag"];
-  var id = data["post"]["current"]["id"]
-  console.log(title + " " + tag + " " + id);
+  var id = data["post"]["current"]["id"];
+  console.log("Webhook triggered");
+  //console.log(title + " " + tag + " " + id);
   messageAdmin.sendMessage("feed_"+tag,id,"Neuer Artikel!","Hey, gerade wurde \""+title+"\" in der Kategorie " + tag + " veröffentlicht!")
   res.status(200).json({ ok: "ok" });
 };
