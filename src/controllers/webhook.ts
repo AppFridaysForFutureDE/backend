890990c251a38ed219fcc9d30ed06542e3f11b7f
Moritz Tietze <moritz.tietze@icloud.com>
2020-03-29 21:08:39 +0200

    Update webhook.ts

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -1,13 +1,12 @@
 import { RequestHandler } from "express";
 import { messageAdmin } from "../app";
+
 export const webhookTriggered: RequestHandler = (req, res) => {
   console.log("Webhook Ghost triggered");
-  console.log(req.body);
   var data = req.body;
   var title = data["post"]["current"]["title"];
   var tag = data["post"]["current"]["primary_tag"]["name"];
   var id = data["post"]["current"]["id"];
-  console.log(title + " " + tag + " " + id);
-  messageAdmin.sendMessage("feed_"+tag,id,"Neuer Artikel!","Hey, gerade wurde \""+title+"\" in der Kategorie " + tag + " veröffentlicht!")
+  messageAdmin.sendMessage("feed_"+tag,id,"Neuer Artikel!","Hey, gerade wurde \""+title+"\" in der Kategorie " + tag + " veröffentlicht!");
   res.status(200).json({ ok: "ok" });
 };
