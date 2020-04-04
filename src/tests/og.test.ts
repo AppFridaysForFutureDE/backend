81bf8464cd3cde1fc9873d29af714739f144423c
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-04 22:59:57 +0200

    move tests to src folder

---
+++ src/tests/og.test.ts
@@ -0,0 +1,19 @@
+import { establishConnection, closeDatabase, clearDatabase } from "./db-handler";
+import { Og } from "../models/ogs";
+
+beforeAll(async () => await establishConnection());
+afterEach(async () => await clearDatabase());
+afterAll(async () => await closeDatabase());
+
+describe("product ", () => {
+  it("can be created correctly", async () => {
+    const newOg = new Og({
+      ogId: "test",
+      name: "MeinOgName"
+    });
+
+    await newOg.save();
+    const insertedUser = await Og.find({});
+    expect(insertedUser[0]["name"]).toEqual("MeinOgName");
+  });
+});
