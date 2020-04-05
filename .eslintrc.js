686dbb8bc4fc4f0b5e6bbb94c8a7bbc59bac4a58
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-05 10:19:26 +0200

    ignore unused next param
    Wir brauchen zwingend die 'next' variable in dem app.ts error handler
    Ich habe die eslint config geändert damit eslint nicht mehr meckert

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
     "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error"
+    "jest/valid-expect": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
   }
 };
