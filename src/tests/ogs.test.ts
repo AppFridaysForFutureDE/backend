8c814c0bb4854e287b25443deeac46053f1bc45c
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-05 09:23:14 +0200

    fix test naming issue

--- src/tests/ogs.test.ts
+++ src/tests/ogs.test.ts
@@ -3,21 +3,21 @@ import {
   closeDatabase,
   clearDatabase
 } from "./db-handler";
-import { Og } from "../models/ogs";
+import { OG } from "../models/ogs";
 
 beforeAll(async () => await establishConnection());
 afterEach(async () => await clearDatabase());
 afterAll(async () => await closeDatabase());
 
-describe("product ", () => {
+describe("og ", () => {
   it("can be created correctly", async () => {
-    const newOg = new Og({
+    const newOg = new OG({
       ogId: "test",
       name: "MeinOgName"
     });
-
     await newOg.save();
-    const insertedUser = await Og.find({});
-    expect(insertedUser[0]["name"]).toEqual("MeinOgName");
+
+    const insertedOg = await OG.find({});
+    expect(insertedOg[0]["name"]).toEqual("MeinOgName");
   });
 });
