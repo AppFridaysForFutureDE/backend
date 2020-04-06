072aab0188835052a9005e35212934e07dd6267e
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-06 23:51:31 +0200

    test og route
    info: there is still an open handle because of the ddos protection
    module

--- src/tests/ogs.test.ts
+++ src/tests/ogs.test.ts
@@ -1,19 +1,22 @@
 import * as dbHandler from "../test-db-handler";
 import { OG } from "../models/ogs";
+import { app } from "../app";
+import request from "supertest";
 
 beforeAll(async () => await dbHandler.establishConnection());
 afterEach(async () => await dbHandler.clearDatabase());
 afterAll(async () => await dbHandler.closeDatabase());
 
-describe("og ", () => {
-  it("can be created correctly", async () => {
+describe("get og route", () => {
+  it("returns the saved og", async () => {
     const newOg = new OG({
       ogId: "test",
       name: "MeinOgName"
     });
     await newOg.save();
 
-    const insertedOg = await OG.find({});
-    expect(insertedOg[0]["name"]).toEqual("MeinOgName");
+    const response = await request(app).get("/api/v1/ogs");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.ogs[0]["name"]).toBe("MeinOgName");
   });
 });
