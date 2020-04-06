885e8045d3fae8813a4ff270e12937ef97a172fd
Nikolai Kircher <nikolai.kircher@gmail.com>
2020-04-06 23:41:06 +0200

    go back to commonjs modules
    i would like to use top level async, but then the other import
    statements break

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,20 @@ import mongoose from "mongoose";
 import { app } from "./app";
 import { startCronJobs } from "./cron";
 
-try {
-  // Connect to database
-  await mongoose.connect("mongodb://localhost:27017/test", {
+// Connect to database
+mongoose
+  .connect("mongodb://fffapp:fffapp@mongo-db:27017/fffapp", {
     useNewUrlParser: true
-  });
+  })
+  .then(
+    () => {
+      // Start listening to requests on port 3000
+      app.listen(3000);
 
-  // Start listening to requests on port 3000
-  app.listen(3000);
-
-  // Start job scheduler
-  startCronJobs();
-} catch (error) {
-  console.log(error);
-}
+      // Start job scheduler
+      startCronJobs();
+    },
+    error => {
+      console.log(error);
+    }
+  );
