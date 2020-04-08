d65a438fe4d42c60be3c05cc07bc306f11e7c0dd
Moritz Tietze <moritz.tietze@icloud.com>
2020-04-08 20:46:09 +0200

    removed ddos

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,7 @@ import { json } from "body-parser";
 import strikeRoutes from "./routes/strikes";
 import ogRoutes from "./routes/ogs";
 import webhookRoutes from "./routes/webhook";
-import Ddos from "ddos";
 import expressStatusMonitor from "express-status-monitor";
-
-//our hoster also has ddos protection
-//maybe we do not need it here?
-//
-//probably need to adjust ddos params burst and limit
-const DoSProtection = new Ddos({ burst: 10, limit: 15 });
-
-//initialise routers; every router needs to use ddos
-strikeRoutes.use(DoSProtection.express);
-ogRoutes.use(DoSProtection.express);
 
 export const app = express();
 app.use(json());
