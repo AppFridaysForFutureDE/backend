285b537b997f702c5a04b2bb6f309285aa30a3ab
Moritz Tietze <moritz.tietze@icloud.com>
2020-04-13 11:22:24 +0200

    edge case tests, linting, renaming

--- src/controllers/strikes.ts
+++ src/controllers/strikes.ts
@@ -6,12 +6,18 @@ export const getStrikes: RequestHandler = (req, res) => {
   const ogId = req.query.ogId;
   //Setzt mindestdatum für streiks auf 0, wenn anfrage festlegt, dass auch vergangene streiks zurückgegeben werden sollen
   //sonst ist das mindestdatum das aktuelle - 24h => so werden auch erst kürzlich beendete streiks noch angezeigt
-  let minDate = (req.query.showPastStrikes == "true") ? 0 : util.toUnixTimestamp(new Date()) - util.day;
+  const minDate =
+    req.query.showPastStrikes == "true"
+      ? 0
+      : util.toUnixTimestamp(new Date()) - util.day;
 
   if (ogId == "" || ogId == null) {
     res.status(400).json({ error: "No OG specified!" });
   } else {
-    Strike.find({ ogId: ogId, date: { $gt: minDate } }, function(err: Error, strikes) {
+    Strike.find({ ogId: ogId, date: { $gt: minDate } }, function(
+      err: Error,
+      strikes
+    ) {
       if (err) return console.error(err);
       res.status(200).json({ ogId: ogId, strikes: strikes });
     });
--- src/tests/strikes.test.ts
+++ src/tests/strikes.test.ts
@@ -2,12 +2,13 @@ import * as dbHandler from "./test-db-handler";
 import { Strike } from "../models/Strikes";
 import { app } from "../app";
 import request from "supertest";
+import * as util from "../utility";
 
 beforeAll(async () => await dbHandler.establishConnection());
 afterEach(async () => await dbHandler.clearDatabase());
 afterAll(async () => await dbHandler.closeDatabase());
 
-describe("get strike route", () => {
+describe("GET /api/v1/strikes", () => {
   beforeEach(async () => {
     const newStrike = new Strike({
       ogId: "test",
@@ -17,28 +18,57 @@ describe("get strike route", () => {
     await newStrike.save();
   });
   it("should return the example object because showPastStrikes is true", async () => {
-    const responsePast = await request(app).get("/api/v1/strikes?ogId=test&showPastStrikes=true");
-    expect(responsePast.statusCode).toBe(200);
+    const responsePast = await request(app).get(
+      "/api/v1/strikes?ogId=test&showPastStrikes=true"
+    );
     expect(responsePast.body.ogId).toBe("test");
-    expect(responsePast.body.strikes.length).toBe(1);
+    expect(responsePast.body.strikes).toHaveLength(1);
     expect(responsePast.body.strikes[0]["name"]).toBe("MeinOgName");
   });
   it("should not return the example object because showPastStrikes isnt given", async () => {
     const responseNoPast = await request(app).get("/api/v1/strikes?ogId=test");
-    expect(responseNoPast.statusCode).toBe(200);
     expect(responseNoPast.body.ogId).toBe("test");
-    expect(responseNoPast.body.strikes.length).toBe(0);
+    expect(responseNoPast.body.strikes).toHaveLength(0);
   });
   it("should not return the example object because showPastStrikes isnt set to true", async () => {
-    const responseNoPast = await request(app).get("/api/v1/strikes?ogId=test&showPastStrikes=false");
-    expect(responseNoPast.statusCode).toBe(200);
+    const responseNoPast = await request(app).get(
+      "/api/v1/strikes?ogId=test&showPastStrikes=false"
+    );
     expect(responseNoPast.body.ogId).toBe("test");
-    expect(responseNoPast.body.strikes.length).toBe(0);
+    expect(responseNoPast.body.strikes).toHaveLength(0);
   });
   it("should not return the example object because showPastStrikes is given, but without any value", async () => {
-    const responseNoPast = await request(app).get("/api/v1/strikes?ogId=test&showPastStrikes=");
-    expect(responseNoPast.statusCode).toBe(200);
+    const responseNoPast = await request(app).get(
+      "/api/v1/strikes?ogId=test&showPastStrikes="
+    );
     expect(responseNoPast.body.ogId).toBe("test");
-    expect(responseNoPast.body.strikes.length).toBe(0);
+    expect(responseNoPast.body.strikes).toHaveLength(0);
+  });
+  it("edge case, should not return the example object because date is a minute too old", async () => {
+    const newStrike = new Strike({
+      ogId: "testEdge",
+      name: "MeinOgName",
+      date: util.toUnixTimestamp(new Date()) - util.day - 60
+    });
+    await newStrike.save();
+    const responsePast = await request(app).get(
+      "/api/v1/strikes?ogId=testEdge"
+    );
+    expect(responsePast.body.ogId).toBe("testEdge");
+    expect(responsePast.body.strikes).toHaveLength(0);
+  });
+  it("edge case, should return the example object because showPastStrikes is true", async () => {
+    const newStrike = new Strike({
+      ogId: "testEdge",
+      name: "MeinOgName",
+      date: util.toUnixTimestamp(new Date()) - util.day + 60
+    });
+    await newStrike.save();
+    const responsePast = await request(app).get(
+      "/api/v1/strikes?ogId=testEdge&showPastStrikes=true"
+    );
+    expect(responsePast.body.ogId).toBe("testEdge");
+    expect(responsePast.body.strikes).toHaveLength(1);
+    expect(responsePast.body.strikes[0]["name"]).toBe("MeinOgName");
   });
 });
