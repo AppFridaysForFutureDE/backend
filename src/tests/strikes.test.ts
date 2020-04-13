e45a8b7c8e0a3ffbf07534337da111aa2e282655
Moritz Tietze <moritz.tietze@icloud.com>
2020-04-13 10:14:14 +0200

    added tests, separated existing tests

--- src/tests/strikes.test.ts
+++ src/tests/strikes.test.ts
@@ -8,21 +8,35 @@ afterEach(async () => await dbHandler.clearDatabase());
 afterAll(async () => await dbHandler.closeDatabase());
 
 describe("get strike route", () => {
-  it("returns the saved strike only when showPastStrikes is true", async () => {
+  beforeEach(async () => {
     const newOg = new Strike({
       ogId: "test",
       name: "MeinOgName",
       date: 567
     });
     await newOg.save();
-
+  });
+  it("should return the example object because showPastStrikes is true", async () => {
     const responsePast = await request(app).get("/api/v1/strikes?ogId=test&showPastStrikes=true");
     expect(responsePast.statusCode).toBe(200);
     expect(responsePast.body.ogId).toBe("test");
     expect(responsePast.body.strikes.length).toBe(1);
     expect(responsePast.body.strikes[0]["name"]).toBe("MeinOgName");
-
+  });
+  it("should not return the example object because showPastStrikes isnt given", async () => {
     const responseNoPast = await request(app).get("/api/v1/strikes?ogId=test");
+    expect(responseNoPast.statusCode).toBe(200);
+    expect(responseNoPast.body.ogId).toBe("test");
+    expect(responseNoPast.body.strikes.length).toBe(0);
+  });
+  it("should not return the example object because showPastStrikes isnt set to true", async () => {
+    const responseNoPast = await request(app).get("/api/v1/strikes?ogId=test&showPastStrikes=false");
+    expect(responseNoPast.statusCode).toBe(200);
+    expect(responseNoPast.body.ogId).toBe("test");
+    expect(responseNoPast.body.strikes.length).toBe(0);
+  });
+  it("should not return the example object because showPastStrikes is given, but without any value", async () => {
+    const responseNoPast = await request(app).get("/api/v1/strikes?ogId=test&showPastStrikes=");
     expect(responseNoPast.statusCode).toBe(200);
     expect(responseNoPast.body.ogId).toBe("test");
     expect(responseNoPast.body.strikes.length).toBe(0);
