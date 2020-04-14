d30b425826ed75485c24ffea8b413b214a796230
Moritz Tietze <moritz.tietze@icloud.com>
2020-04-14 23:36:00 +0200

    Added social meta data

--- src/controllers/share.ts
+++ src/controllers/share.ts
@@ -1,5 +1,19 @@
 import { RequestHandler } from "express";
+import nodeFetch from "node-fetch";
 
-export const sharePost: RequestHandler = (req, res) => {
-  res.redirect(`https://appforfuture.page.link/?amv=100&apn=de.fridaysforfuture.app&ibi=de.fridaysforfuture.app.official&imv=1.0.0&isi=1506077796&link=https://app.fffutu.re/feed/${req.params.post}&ofl=https://app.fffutu.re/download`);
+export const sharePost: RequestHandler = async (req, res) => {
+  const response = await nodeFetch(`https://app.fffutu.re/ghost/api/v3/content/posts/${req.params.posts}/?key=${process.env.GHOST_CONTENT_KEY}`);
+  let data = [];
+  let title: String = "Error";
+  let desc: String = "Error";
+  let imageUrl: String = "Error";
+  try {
+    data = await response.json();
+    title = data[0]["title"];
+    desc = data[0]["custom_excerpt"];
+    imageUrl = data[0]["feature_image"];
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect(`https://appforfuture.page.link/?amv=100&apn=de.fridaysforfuture.app&ibi=de.fridaysforfuture.app.official&imv=1.0.0&isi=1506077796&link=https://app.fffutu.re/feed/${req.params.post}&ofl=https://app.fffutu.re/download&st=${title}&sd=${desc}&si=${imageUrl}`);
 };
