30df16ba6515a621dbcbffeb05f7e7df4e88f1cb
Moritz Tietze <moritz.tietze@icloud.com>
2020-04-14 22:17:20 +0200

    added firebase redirection

--- src/controllers/share.ts
+++ src/controllers/share.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express";
 
 export const sharePost: RequestHandler = (req, res) => {
-  res.status(200).json({ post: req.params.post });
+  res.redirect(`https://appforfuture.page.link/?amv=32&apn=de.fridaysforfuture.app&ibi=de.fridaysforfuture.app.official&imv=0.3.2&isi=1506077796&link=https://app.fffutu.re/post/${req.params.post}`);
 };
