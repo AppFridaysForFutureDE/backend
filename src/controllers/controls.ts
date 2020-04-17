1d16eb2d7dba1ad91ed17c7cd54d49efad50a236
Moritz Tietze <moritz.tietze@icloud.com>
2020-04-17 18:10:55 +0200

    added responses

--- src/controllers/controls.ts
+++ src/controllers/controls.ts
@@ -1,13 +1,16 @@
 import { RequestHandler } from "express";
 
 export const firebaseStatus: RequestHandler = (req, res) => {
-
+  console.log("Firebase status requested")
+  res.status(501);
 };
 
 export const createBackup: RequestHandler = (req, res) => {
-
+  console.log("Firebase status requested")
+  res.status(501);
 };
 
 export const populateDB: RequestHandler = (req, res) => {
-
+  console.log("Firebase status requested")
+  res.status(501);
 };
