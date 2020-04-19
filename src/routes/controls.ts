4c417763b6d11a5b5c60c403d31a3943fdefaafc
Moritz Tietze <moritz.tietze@icloud.com>
2020-04-19 21:51:45 +0200

    Update controls.ts

--- src/routes/controls.ts
+++ src/routes/controls.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { firebaseStatus } from "../controllers/controls";
-import { createBackup } from "../controllers/controls";
 import { populateDB } from "../controllers/controls";
 
 const router = Router();
