3a0a77b65b3649b22be2b56847d172a08b16b53c
Moritz Tietze <moritz.tietze@icloud.com>
2020-05-15 16:45:22 +0200

    removed debug routes

--- src/routes/controls.ts
+++ src/routes/controls.ts
@@ -1,14 +1,10 @@
 import { Router } from "express";
 import { firebaseStatus } from "../controllers/controls";
 import { populateDB } from "../controllers/controls";
-import { debugPost } from "../controllers/controls";
-import { debugGet } from "../controllers/controls";
 
 const router = Router();
 
 router.get("/firebaseStatus", firebaseStatus);
 router.post("/populateDB", populateDB);
-router.get("/debug", debugGet);
-router.post("/debug", debugPost);
 
 export default router;
