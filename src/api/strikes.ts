7e814bab402483694d10b164f532023e6da46cc6
Moritz Tietze <moritz.tietze@icloud.com>
2020-05-18 14:37:04 +0200

    debug

--- src/api/strikes.ts
+++ src/api/strikes.ts
@@ -5,6 +5,7 @@ import nodeFetch from "node-fetch";
 
 //retrieves Strikes from website api and saves them to mongodb
 export async function retrieveStrikes(): Promise<void> {
+  console.log("strikes");
   const response = await nodeFetch(/*`${process.env.WEBSITE_URL}/strike`*/"https://api.jsonbin.io/b/5ec27b662bb52645e553112f/1");
   let data = [];
   try {
