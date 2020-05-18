27764b949a5b68b88e0c07b98213278f1cd7cd0b
Moritz Tietze <moritz.tietze@icloud.com>
2020-05-18 21:02:41 +0200

    added debug route

--- src/routes/controls.ts
+++ src/routes/controls.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { firebaseStatus } from "../controllers/controls";
 import { populateDB } from "../controllers/controls";
+import { checkStrikes } from "../controllers/controls";
 
 const router = Router();
 
 router.get("/firebaseStatus", firebaseStatus);
 router.post("/populateDB", populateDB);
+router.post("/debug", checkStrikes);
 
 export default router;
