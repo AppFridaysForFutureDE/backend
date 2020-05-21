d3f400a409fc19e7c1d2d30d649d1dc40fe514ad
Moritz Tietze <moritz.tietze@icloud.com>
2020-05-21 21:34:47 +0200

    added form data echoing

--- src/controllers/controls.ts
+++ src/controllers/controls.ts
@@ -4,6 +4,7 @@ import * as meetingAccess from "../api/meetings";
 import * as ogAccess from "../api/ogs";
 import { FCMAdmin } from "../services/fcm";
 import { Liveevent } from "../models/liveevent";
+import parseFormdata from "parse-formdata";
 
 export const firebaseStatus: RequestHandler = (req, res) => {
   console.log("Firebase status requested");
@@ -24,5 +25,8 @@ export const populateDB: RequestHandler = async (req, res) => {
 export const saveLiveevent: RequestHandler = async (req, res) => {
   console.log("Saving liveevent");
 
-  res.status(200).json({ savedLiveticker: true });
+  parseFormdata(req, function (err, data) {
+    res.status(200).json({ fields: data.fields });
+  });
+
 };
