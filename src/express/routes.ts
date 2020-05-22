2d3e0a5ea387c1b92dda0ab98279ef53fa510390
Moritz Tietze <moritz.tietze@icloud.com>
2020-05-22 09:25:49 +0200

    moved express related files

--- src/express/routes.ts
+++ src/express/routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { populateDB, saveLiveevent, firebaseStatus } from "./endpoints/controls";
-import { getLiveevent, getOGs, getStrikes } from "./endpoints/api";
-import { sharePost } from "./endpoints/share";
-import { webhookTriggered } from "./endpoints/webhook";
+import { populateDB, saveLiveevent, firebaseStatus } from "../endpoints/controls";
+import { getLiveevent, getOGs, getStrikes } from "../endpoints/api";
+import { sharePost } from "../endpoints/share";
+import { webhookTriggered } from "../endpoints/webhook";
 
 export const controlsRoutes = Router();
 export const liveeventRoutes = Router();
