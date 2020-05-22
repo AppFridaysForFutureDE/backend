e1a2927272cc2818725ed199e42276541d6fc8b1
Moritz Tietze <moritz.tietze@icloud.com>
2020-05-22 09:27:50 +0200

    fiexd foreach todo

--- src/endpoints/webhook.ts
+++ src/endpoints/webhook.ts
@@ -9,16 +9,16 @@ export const webhookTriggered: RequestHandler = (req, res) => {
   const tags = data["post"]["current"]["tags"];
   let topic = "";
   let push = false;
-  //TODO: foreach
-  let i = 0;
-  for (i = 0; i < tags.length; i++) {
-    if (tags[i]["description"] == "Themenbereich") {
-      topic = tags[i]["name"];
+
+  tags.array.forEach(tag => {
+    if (tag["description"] == "Themenbereich") {
+      topic = tag["name"];
     }
-    if (tags[i]["name"] == "Push") {
+    if (tag["name"] == "Push") {
       push = true;
     }
-  }
+  });
+  
   //TODO: String Templates
   if (push) {
     FCMAdmin.getInstance().sendMessage(
