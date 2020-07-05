19b17a5642a094f2892a4e16e7e2689757ca3129
Moritz Tietze <moritz.tietze@icloud.com>
2020-07-05 13:17:15 +0200

    debug logs

--- src/endpoints/views.ts
+++ src/endpoints/views.ts
@@ -3,11 +3,13 @@ import { FCMAdmin } from "../services/fcm";
 
 export const loginView: RequestHandler = async (req, res) => {
     //redirect to controls if good session id
+    console.log("login page");
     res.render("login");
 }
 
 export const controlsView: RequestHandler = async (req, res) => {
     //redirect to login if wrong/missing session id
+    console.log("controls page");
     res.render("controls", {
         firebaseStatus: FCMAdmin.getInstance().getStatus(),
         admin: false
