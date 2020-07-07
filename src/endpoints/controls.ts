6cd9f98ce9756982dc404c85d1f4bad32c2274b6
Moritz Tietze <moritz.tietze@icloud.com>
2020-07-07 21:33:15 +0200

    fix

--- src/endpoints/controls.ts
+++ src/endpoints/controls.ts
@@ -69,7 +69,7 @@ export const remove: RequestHandler = async (req, res) => {
 
 export const create: RequestHandler = async (req, res) => {
   let username = req.body.username;
-  let admin: boolean = req.body.admin;
+  let admin: boolean = (req.body.admin == "on");
   let sessID = req.cookies["fff_sessionid"];
   let success = await UserManager.createUser({username, admin}, sessID);
   res.redirect("/views/panel/controls");
