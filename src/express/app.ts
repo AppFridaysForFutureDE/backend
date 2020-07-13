34b44f329767e7ee2d5a07bbf1f6429ff3cd015d
Moritz Tietze <moritz.tietze@icloud.com>
2020-07-13 08:37:37 +0200

    removed status monitor

--- src/express/app.ts
+++ src/express/app.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import { json } from "body-parser";
 import cookieParser from "cookie-parser";
-import expressStatusMonitor from "express-status-monitor";
 import path from "path";
 
 //routes
@@ -24,19 +23,6 @@ app.use(express.urlencoded());
 app.set("views", path.join(__dirname, "../../src/views"));
 app.set("view engine", "ejs");
 
-//Check Session for status
-app.use("/admin/status", async function(err: Error, req: Request, res: Response, next) { // eslint-disable-line
-  console.log("middleware for session checking invoked");
-  const sessID = req.cookies["fff_sessionid"];
-  if ((await UserManager.checkSessionID(sessID)).valid) {
-    console.log("success for monitor");
-    next();
-  } else {
-    console.log("redirecting to login");
-    res.redirect("/views/panel/login");
-  }
-})
-
 //API Routes
 app.use("/api/v1/strikes", strikeRoutes);
 app.use("/api/v1/ogs", ogRoutes);
@@ -50,7 +36,6 @@ app.use("/p", shareRoutes);
 
 //Controls Routes
 app.use("/admin/controls", controlsRoutes);
-app.use("/admin/status", expressStatusMonitor({ path: "/admin/status" }));
 
 //View Routes
 app.use("/views", viewRoutes);
