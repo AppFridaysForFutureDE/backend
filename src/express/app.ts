1d392aa1927ab2f27d0d410a3f7531892c6c8a68
Moritz Tietze <moritz.tietze@icloud.com>
2020-07-13 08:32:24 +0200

    debug log

--- src/express/app.ts
+++ src/express/app.ts
@@ -26,10 +26,13 @@ app.set("view engine", "ejs");
 
 //Check Session for status
 app.use("/admin/status", async function(err: Error, req: Request, res: Response, next) { // eslint-disable-line
+  console.log("middleware for session checking invoked");
   const sessID = req.cookies["fff_sessionid"];
   if ((await UserManager.checkSessionID(sessID)).valid) {
+    console.log("success for monitor");
     next();
   } else {
+    console.log("redirecting to login");
     res.redirect("/views/panel/login");
   }
 })
