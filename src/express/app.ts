8399ab345cf362fbdeae4ce60d9bbc292e131828
Moritz Tietze <moritz.tietze@icloud.com>
2020-07-28 23:13:35 +0200

    Revert "static serving for assets"
    This reverts commit 5c96333b6438675d8135e6d900d896a65074931d.

--- src/express/app.ts
+++ src/express/app.ts
@@ -44,8 +44,6 @@ app.use(async function(req: Request, res: Response, next) {
   }
 });
 
-app.use(express.static('../assets'));
-
 //API Routes
 app.use("/api/v1/strikes", strikeRoutes);
 app.use("/api/v1/ogs", ogRoutes);
