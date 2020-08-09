75e9e501cf9e5dee9cec4892ebf59848ca47042e
Moritz Tietze <moritz.tietze@icloud.com>
2020-08-10 00:43:07 +0200

    fix for empty tags

--- src/endpoints/actions.ts
+++ src/endpoints/actions.ts
@@ -42,7 +42,7 @@ export const addSlogan: RequestHandler = async (req, res) => {
       res.redirect("/views/panel/controls".concat("?err=true"));
       return;
     }
-    const result = await Slogan.create({ title: req.body.title, text: req.body.text, tags: req.body.tags.split(",").map(t => { return t.trim(); }) });
+    const result = await Slogan.create({ title: req.body.title, text: req.body.text, tags: req.body.tags.split(",").map(t => { return t.trim() == "" ? null : t.trim(); }) });
     res.redirect("/views/panel/controls".concat(result ? "" : "?err=true"));
   } else {
     res.redirect("/views/panel/login");
