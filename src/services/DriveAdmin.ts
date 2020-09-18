07eb5c8194d91c84340f0aec7989089863ece1e3
Moritz Tietze <moritz.tietze@icloud.com>
2020-09-18 21:49:10 +0200

    added authorization code and volume

--- src/services/DriveAdmin.ts
+++ src/services/DriveAdmin.ts
@@ -1,7 +1,88 @@
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+
 export class DriveAdmin {
 
-    public async loadImageById(id: string): Promise<boolean> {
-        return false;
+    // If modifying these scopes, delete token.json.
+    private SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    private TOKEN_PATH = 'token.json';
+
+    private authDone = false;
+
+    public async init() {
+        // Load client secrets from a local file.
+        fs.readFile('credentials.json', (err, content) => {
+            if (err) return console.log('Error loading client secret file:', err);
+            // Authorize a client with credentials, then call the Google Drive API.
+            this.authorize(JSON.parse(content), () => {
+                this.authDone = true;
+            });
+        });
     }
-    
+
+    public async loadImageById(id: string): Promise<boolean> {
+        if (!this.authDone) {
+            await this.init()
+        }
+        console.log("AUTHORIZED GOOGLE DRIVE API");
+        return true;
+    }
+
+
+
+    /**
+     * Create an OAuth2 client with the given credentials, and then execute the
+     * given callback function.
+     * @param {Object} credentials The authorization client credentials.
+     * @param {function} callback The callback to call with the authorized client.
+     */
+    private authorize(credentials, callback) {
+        const { client_secret, client_id, redirect_uris } = credentials.installed;
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]);
+
+        // Check if we have previously stored a token.
+        fs.readFile(this.TOKEN_PATH, (err, token) => {
+            if (err) return this.getAccessToken(oAuth2Client, callback);
+            oAuth2Client.setCredentials(JSON.parse(token));
+            callback(oAuth2Client);
+        });
+    }
+
+    /**
+     * Get and store new token after prompting for user authorization, and then
+     * execute the given callback with the authorized OAuth2 client.
+     * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+     * @param {getEventsCallback} callback The callback for the authorized client.
+     */
+    private getAccessToken(oAuth2Client, callback) {
+        const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: this.SCOPES,
+        });
+        console.log('Authorize this app by visiting this url:', authUrl);
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        rl.question('Enter the code from that page here: ', (code) => {
+            rl.close();
+            oAuth2Client.getToken(code, (err, token) => {
+                if (err) return console.error('Error retrieving access token', err);
+                oAuth2Client.setCredentials(token);
+                // Store the token to disk for later program executions
+                fs.writeFile(this.TOKEN_PATH, JSON.stringify(token), (err) => {
+                    if (err) return console.error(err);
+                    console.log('Token stored to', this.TOKEN_PATH);
+                });
+                callback(oAuth2Client);
+            });
+        });
+    }
+
+
 }
