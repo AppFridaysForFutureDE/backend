d54a07c065cdb708b2fa7a6d3200096ebeffe046
Moritz Tietze <moritz.tietze@icloud.com>
2020-09-18 16:05:36 +0200

    debug log

--- src/data/ogcontent.ts
+++ src/data/ogcontent.ts
@@ -2,10 +2,18 @@ import { OG } from "../models/ogsModel";
 import { SpreadsheetAdmin } from "../services/SpreadsheetAdmin";
 
 export async function getOGContent(): Promise<void> {
+
+    console.log("get og content");
+    console.log("creating spreadsheetadmin");
     let ogContentAdmin = new SpreadsheetAdmin(process.env.OGCONTENT_SPREADSHEET_ID || "")
+    console.log("loading doc info");
     await ogContentAdmin.loadDocumentInfo();
+    console.log("getting rows");
     let rows = await ogContentAdmin.getRows();
+    console.log("starting to save rows");
     rows.forEach(row => {
+        console.log("saving row:");
+        console.log(row);
         OG.findOneAndUpdate({ name: row["Deine OG"]}, {
             imageLink: row["Lade dein Bild ein. Bitte als Quadrat!"],
             infoTitle: row["Überschrift"],
