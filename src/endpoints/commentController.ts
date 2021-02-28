b51fe54c13b8b7b7d78568b3e8ef34184a5ba14c
Nikolai Kircher <nikolai.kircher@gmail.com>
2021-02-28 17:44:41 +0100

    catch exceptions in create comment

--- src/endpoints/commentController.ts
+++ src/endpoints/commentController.ts
@@ -2,13 +2,21 @@ import { RequestHandler } from "express";
 import { Comment } from "../models/commentModel";
 
 export const createComment: RequestHandler = async (req, res) => {
-  const success = await Comment.create({
-    name: req.body.name,
-    age: req.body.age,
-    content: req.body.content,
-    publish: req.body.publish,
-    articleReference: req.body.articleReference,
-  });
-  // TODO: redirect to new success page
-  res.redirect("/views/panel/controls".concat(success ? "" : "?err=true"));
+  try {
+    console.log("saving comment");
+    await Comment.create({
+      name: req.body.name,
+      age: req.body.age,
+      content: req.body.content,
+      publish: req.body.publish,
+      articleReference: req.body.articleReference,
+    });
+
+    // TODO: redirect to new success page
+    /* res.redirect("/views/panel/controls"); */
+    res.send(200);
+  } catch (e) {
+    console.log(`Error while saving comment ${e}`);
+    res.send(442);
+  }
 };
